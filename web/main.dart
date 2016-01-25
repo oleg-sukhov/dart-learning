a009f9646e468a0f2dbd12613adf5a0516de3541
oleg-sukhov <oleg.sukhov@email.ua>
2016-01-26 00:40:07 +0200

    Dart packList application - add toogle feature

--- web/main.dart
+++ web/main.dart
@@ -12,18 +12,17 @@ main() {
   InputElement input = new Element.tag("input");
   input.placeholder = "Enter an Item";
   input.id = "txt-item";
+  input.onKeyPress.listen((event) {
+    if(event.keyCode == 13) {
+      addItem(event);
+    }
+  });
   document.body.children.add(input);
 
   ButtonElement button = new Element.tag("button");
   button.appendText("Add");
   button.id = "add-btn";
-  button.onClick.listen((event) {
-    var newItem = input.value;
-    var newElement = new Element.html("<div class='item'>${newItem}<div>");
-    itemContainer.children.add(newElement);
-    input.value = "";
-
-  });
+  button.onClick.listen((event) => addItem(event));
   document.body.children.add(button);
 
   itemContainer.id = "items";
@@ -31,5 +30,45 @@ main() {
   itemContainer.style.border = "1px solid black";
   itemContainer.appendHtml("&nbsp;");
   document.body.children.add(itemContainer);
+
+}
+
+addItem(Event event) {
+  InputElement input = querySelector("input");
+  PackItem newItem = new PackItem(input.value);
+  var itemContainer = querySelector("#items");
+  itemContainer.children.add(newItem.uiElement);
+  input.value = "";
+
+}
+
+class PackItem {
+  var itemText;
+  DivElement _uiElement;
+  var _isPacked = false;
+
+  PackItem(this.itemText);
+
+  bool get isPacked => _isPacked;
+
+  set isPacked(value) {
+    _isPacked = value;
+
+    if(_isPacked == true) {
+      uiElement.classes.add("packed");
+    } else {
+      uiElement.classes.remove("packed");
+    }
+  }
+
+  DivElement get uiElement {
+    if(_uiElement == null) {
+      _uiElement = new Element.tag("div");
+      _uiElement.classes.add("item");
+      _uiElement.text = itemText;
+      _uiElement.onClick.listen((event) => isPacked = !isPacked);
+    }
+    return _uiElement;
+  }
 
 }
